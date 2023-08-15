9acf00ed62a6fb6cc8a1dd00b3bd286dade9f050
LuongXuanNhat <xuannhat1932002@gmail.com>
2023-08-15 15:21:48 +0700

    Oke

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProjectAngular';
+  public name = "Lương Xuân Nhất";
+  public age = 18;
+
+  public resetname(): void {
+    console.log('resetname');
+  }
 }
